371778ae1c0de5367916ed5bbd3ce6145ea65fb6
Abbas Radhi <abbe.radhi11@gmail.com>
2024-12-03 23:02:47 +0100

    Enable CORS

--- src/main/java/com/abberadhi/mc_forum/config/SecurityConfig.java
+++ src/main/java/com/abberadhi/mc_forum/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,25 +16,25 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    
+
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .authorizeHttpRequests(requests -> {
-            requests.requestMatchers("/api/auth/**").permitAll();
-            requests.requestMatchers("/api/**").authenticated();        
-            requests.requestMatchers("/**").permitAll();
-        })
-        .sessionManagement(management -> management
+                .authorizeHttpRequests(requests -> {
+                    requests.requestMatchers("/api/auth/**").permitAll();
+                    requests.requestMatchers("/api/**").authenticated();
+                    requests.requestMatchers("/**").permitAll();
+                })
+                .sessionManagement(management -> management
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authenticationProvider(authenticationProvider)
-        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-        .cors().and()
-        .csrf().disable();
-
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                // .cors().and()
+                .cors(Customizer.withDefaults())
+                .csrf().disable();
 
         return http.build();
     }
