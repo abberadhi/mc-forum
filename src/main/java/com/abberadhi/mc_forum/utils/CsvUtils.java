75893863a823af02491f89901e5ccc43dcb87e7b
Abbas Radhi <abbe.radhi11@gmail.com>
2024-12-07 19:58:16 +0100

    Add mock data

--- src/main/java/com/abberadhi/mc_forum/utils/CsvUtils.java
+++ src/main/java/com/abberadhi/mc_forum/utils/CsvUtils.java
@@ -1,16 +1,54 @@
 package com.abberadhi.mc_forum.utils;
 
 import java.io.FileReader;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 import com.abberadhi.mc_forum.model.BikeModelEntity;
+import com.abberadhi.mc_forum.model.CommentEntity;
+import com.abberadhi.mc_forum.model.PostEntity;
+import com.abberadhi.mc_forum.model.TagEntity;
+import com.abberadhi.mc_forum.model.UserEntity;
+import com.abberadhi.mc_forum.repository.BikeModelRepository;
+import com.abberadhi.mc_forum.repository.CommentRepository;
+import com.abberadhi.mc_forum.repository.PostRepository;
+import com.abberadhi.mc_forum.repository.TagRepository;
+import com.abberadhi.mc_forum.repository.UserRepository;
 import com.opencsv.CSVReader;
 
+@Component
 public class CsvUtils {
 
-    public static List<BikeModelEntity> readBikesFromCsv(String filePath) {
-        List<BikeModelEntity> bikes = new ArrayList();
+    private final PasswordEncoder passwordEncoder;
+    private final BikeModelRepository bikeModelRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final TagRepository tagRepository;
+
+    public CsvUtils(
+            PasswordEncoder passwordEncoder,
+            BikeModelRepository bikeModelRepository,
+            UserRepository userRepository,
+            TagRepository tagRepository,
+            PostRepository postRepository,
+            CommentRepository commentRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.bikeModelRepository = bikeModelRepository;
+        this.userRepository = userRepository;
+        this.tagRepository = tagRepository;
+        this.postRepository = postRepository;
+        this.commentRepository = commentRepository;
+    }
+
+    public List<BikeModelEntity> readBikesFromCsv(String filePath) {
+        List<BikeModelEntity> bikes = new ArrayList<>();
         try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
             String[] line;
             while ((line = reader.readNext()) != null) {
@@ -24,4 +62,94 @@ public class CsvUtils {
         }
         return bikes;
     }
+
+    public List<UserEntity> readUsersFromCsv(String filePath) {
+        List<UserEntity> users = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                UserEntity u = new UserEntity();
+                u.setUsername(line[1].strip());
+                u.setDescription(line[2].strip());
+                u.setDateJoined(LocalDateTime.now());
+                u.setPassword(passwordEncoder.encode("pass"));
+                Long bikeModelId = Long.parseLong(line[5]);
+                u.setBikeModelEntity(bikeModelRepository.findById(bikeModelId).orElse(null));
+                users.add(u);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public List<TagEntity> readTagsFromCsv(String filePath) {
+        List<TagEntity> tags = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                TagEntity t = new TagEntity();
+                t.setName(line[0]);
+                tags.add(t);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tags;
+    }
+
+    public List<PostEntity> readPostsFromCsv(String filePath) {
+        List<PostEntity> posts = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                PostEntity p = new PostEntity();
+                p.setTitle(line[0]);
+                p.setContent(line[1]);
+                p.setUser(userRepository.findById(Integer.parseInt(line[2])).orElse(null));
+                Set<TagEntity> tags = new HashSet<>();
+                String[] tagNames = line[3].split(";");
+                for (String tag : tagNames) {
+                    TagEntity tagEntity = tagRepository.findByName(tag).orElse(null);
+                    if (tagEntity == null) {
+                        continue;
+                    }
+                    tags.add(tagEntity);
+                }
+                p.setTags(tags);
+                posts.add(p);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    public List<CommentEntity> readCommentsFromCsv(String filePath) {
+        List<CommentEntity> comments = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                CommentEntity comment = new CommentEntity();
+                comment.setCommentContent(line[0].strip());
+                comment.setPostEntity(postRepository.findById(Long.parseLong(line[1])).orElse(null));
+                comment.setUserEntity(userRepository.findById(Integer.parseInt(line[2])).orElse(null));
+
+                // Handle parent comment reference
+                if (line[3] != null && !line[3].isEmpty()) {
+                    comment.setParentCommentEntity(commentRepository.findById(Long.parseLong(line[3])).orElse(null));
+                }
+
+                // Handle upvote count
+                comment.setUpvoteCount(Integer.parseInt(line[4].strip()));
+
+                commentRepository.save(comment);
+                comments.add(comment);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return comments;
+    }
+
 }
--- src/main/java/com/abberadhi/mc_forum/utils/DatabaseInitializer.java
+++ src/main/java/com/abberadhi/mc_forum/utils/DatabaseInitializer.java
@@ -7,22 +7,46 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.abberadhi.mc_forum.model.BikeModelEntity;
+import com.abberadhi.mc_forum.model.CommentEntity;
+import com.abberadhi.mc_forum.model.PostEntity;
+import com.abberadhi.mc_forum.model.TagEntity;
+import com.abberadhi.mc_forum.model.UserEntity;
 import com.abberadhi.mc_forum.repository.BikeModelRepository;
+import com.abberadhi.mc_forum.repository.CommentRepository;
+import com.abberadhi.mc_forum.repository.PostRepository;
+import com.abberadhi.mc_forum.repository.TagRepository;
+import com.abberadhi.mc_forum.repository.UserRepository;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
 
     private final BikeModelRepository bikeModelRepository;
-
-    public DatabaseInitializer(BikeModelRepository bikeModelRepository) {
-        this.bikeModelRepository = bikeModelRepository;
-    }
+    private final UserRepository userRepository;
+    private final TagRepository tagRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final CsvUtils csvUtils;
 
     @Override
     public void run(String... args) throws Exception {
-        String filePath = new ClassPathResource("data/bikes-list.csv").getFile().getAbsolutePath();
+        String filePathBikes = new ClassPathResource("data/bikes-list.csv").getFile().getAbsolutePath();
+        String filePathUsers = new ClassPathResource("data/users-list.csv").getFile().getAbsolutePath();
+        String filePathTags = new ClassPathResource("data/tag-list.csv").getFile().getAbsolutePath();
+        String filePathPosts = new ClassPathResource("data/post-list.csv").getFile().getAbsolutePath();
+        String filePathComments = new ClassPathResource("data/comments-list.csv").getFile().getAbsolutePath();
+        populateBikeTable(filePathBikes);
+        populateUserTable(filePathUsers);
+        populateTagsTable(filePathTags);
+        populatePostsTable(filePathPosts);
+        populateCommentsTable(filePathComments);
+    }
 
-        List<BikeModelEntity> bikeEntities = CsvUtils.readBikesFromCsv(filePath);
+    private void populateBikeTable(String filePath) {
+        List<BikeModelEntity> bikeEntities = csvUtils.readBikesFromCsv(filePath);
 
         if (bikeModelRepository.count() != 0) {
             return;
@@ -31,5 +55,41 @@ public class DatabaseInitializer implements CommandLineRunner {
         for (BikeModelEntity bike : bikeEntities) {
             bikeModelRepository.save(bike);
         }
+    }
+
+    private void populateUserTable(String filePath) {
+        if (userRepository.count() != 0) {
+            return;
+        }
+
+        List<UserEntity> userEntities = csvUtils.readUsersFromCsv(filePath);
+        userEntities.forEach(userRepository::save);
+    }
+
+    private void populateTagsTable(String filePath) {
+        if (tagRepository.count() != 0) {
+            return;
+        }
+
+        List<TagEntity> tagEntities = csvUtils.readTagsFromCsv(filePath);
+        tagEntities.forEach(tagRepository::save);
+    }
+
+    private void populatePostsTable(String filePath) {
+        if (postRepository.count() != 0) {
+            return;
+        }
+
+        List<PostEntity> postEntities = csvUtils.readPostsFromCsv(filePath);
+        postEntities.forEach(postRepository::save);
+    }
+
+    private void populateCommentsTable(String filePath) {
+        if (commentRepository.count() != 0) {
+            return;
+        }
+
+        List<CommentEntity> commentEntities = csvUtils.readCommentsFromCsv(filePath);
+        // commentEntities.forEach(commentRepository::save);
     }
 }
